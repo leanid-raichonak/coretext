80033d0dc7d239e487872dcaa597b328f5f30dc7
leonidrai <leonidrai@synesis.ru>
2013-02-28 16:55:32 +0300

    Initial Commit

---
+++ coretext/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  coretext
+//
+//  Created by Leonid on 28.02.13.
+//  Copyright (c) 2013 Leonid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ coretext/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  coretext
+//
+//  Created by Leonid on 28.02.13.
+//  Copyright (c) 2013 Leonid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
